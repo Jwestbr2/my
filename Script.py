7dd38b6f37ba91b379a7231ee62fbd044938c2ef
Jwestbr2 <92010115+Jwestbr2@users.noreply.github.com>
2023-05-17 14:42:15 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,21 +1,17 @@
 class script(object):
-    START_TXT = """👋 Hello {},
-
-I can give any Movie and TV Series to auto filter method. 🥳
-
-All you have to do is add me to a group and give me admin. 😌
-
-I will take care of the rest. 😎"""
+    START_TXT = """**𝙷𝙴𝙻𝙾 Hacker, 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 Rajfiles Movies Search Bot.
+𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍.Gᴇᴛ ᴀʟʟ Mᴏᴠɪᴇs/ TV & Wᴇʙ sᴇʀɪᴇs/ Fᴀᴄᴛs/ Sᴏɴɢs etc.
+ᴀᴛ ᴏɴᴇ ᴘʟᴀᴄᴇ.😊Mᴏᴠɪᴇ & Sᴇʀɪᴇs ʙʀᴏᴡsɪɴɢ ʜᴀs ɴᴇᴠᴇʀ ʙᴇᴇɴ ᴛʜɪs ᴇᴀsʏ."""
 
     MY_ABOUT_TXT = """★ Server: <a href=https://www.heroku.com>Heroku</a>
 ★ Database: <a href=https://www.mongodb.com>MongoDB</a>
 ★ Language: <a href=https://www.python.org>Python</a>
 ★ Library: <a href=https://pyrogram.org>Pyrogram</a>"""
 
-    MY_OWNER_TXT = """★ Name: Hansaka Anuhas
-★ Username: @Hansaka_Anuhas
-★ ID: <code>5493832202</code>
-★ Country: Sri Lanka 🇱🇰"""
+    MY_OWNER_TXT = """★ Name: 『sᴀᴛʏᴀᴍ』
+★ Username: @Mr_majnu72
+★ ID: <code>5097836954</code>
+★ Country: INDIA """
 
     STATUS_TXT = """🗂 Total Files: <code>{}</code>
 👤 Total Users: <code>{}</code>
